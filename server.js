6cc5e5976106f410f075d3027a45b0d69749de44
Chie <akiba.chie@gmail.com>
2016-07-28 15:56:07 -0700

    Was able to show the artist and related artists and access top tracks per related artists but not show them

--- server.js
+++ server.js
@@ -26,42 +26,42 @@ app.get('/search/:name', function (req, res) {
         limit: 1,
         type: 'artist'
     });
-
+    //    var result = function (artist) {
+    //        artist
+    //        console.log('Did everything work as planned?');
+    //    };
     searchReq.on('end', function (item) {
         var artist = item.artists.items[0];
         var relatedArtist = getFromApi('artists/' + artist.id + '/related-artists');
 
         relatedArtist.on('end', function (relatedItem) {
             // related artists > 1
-            // loop
-            var relatedArtists = relatedItem.artists;
-            //            console.log(relatedItem, "RELATED!!!!");
-            // Don't use For loop...
-            // Takes a lot of time :-)
-            for (var i = 0; i < relatedArtists.length; i++) {
-
-                //                console.log("Reached here");
-                var artistTopTrack = getFromApi('artists/' + relatedArtists[i].id + '/top-tracks', {
-                    country: 'US'
-                });
-                //            console.log('Artist top track:', artistTopTrack);
-
-                artistTopTrack.on('end', function (topTrack) {
-                    console.log(topTrack, "TOP Track");
-                    artist = item.tracks;
-                    //                    console.log('artist', artist);
-                    // THIS is the end
-                    res.json(artist);
-                });
-            }
 
             artist.related = relatedItem.artists;
-            //            res.json(artist);
+            var relatedArtists = artist.related;
+            // Don't use For loop...
+            // Takes a lot of time :-)
+
+            async.map(artist.related, function (relatedArtist, result) {
+                var artistTopTrack = getFromApi('artists/' + relatedArtists.id + '/top-tracks', {
+                    country: 'US'
+                });
+                artistTopTrack.on('end', function (topTrack) {
+                    artist.related = item.tracks
+                    console.log('TRACKS', item.tracks);
+                    res.json(artist);
+                });
+                // item ->  object representing an artist
+                // item.tracks =
+
+            });
+
         });
 
         relatedArtist.on('error', function (code) {
             res.sendStatus(code);
         });
+
     });
 
     searchReq.on('error', function (code) {
